06d1bd62c1ee992288160413e7279c264a7cb943
LazyLexa <zlexa90@mail.ru>
2016-08-10 15:34:54 +0300

    дописал ещё "привет вселеннная"

--- HelloWorld.java
+++ HelloWorld.java
@@ -1,4 +1,4 @@
-/*
+﻿/*
  * To change this license header, choose License Headers in Project Properties.
  * To change this template file, choose Tools | Templates
  * and open the template in the editor.
@@ -12,5 +12,6 @@ package helloworld;
 public class HelloWorld {
     public static void main(String[] args) {
         System.out.println("Hello, World!");
+	System.out.println("Hello, Universe!");
     }    
 }
