fb1d14189b7b1c032268eacfb49737e251790ca7
LazyLexa <zlexa90@mail.ru>
2016-08-15 14:19:13 +0300

    проставил точки с запятой

--- HelloWorld.java
+++ HelloWorld.java
@@ -11,8 +11,8 @@ package helloworld;
  */
 public class HelloWorld {
     public static void main(String[] args) {
-        System.out.println("Hello, World!");
-	System.out.println("Hello, Universe!");
-	System.out.println("Hello, All!");
+        System.out.println("Hello, World!;");
+	System.out.println("Hello, Universe!;");
+	System.out.println("Hello, All!;");
     }    
 }
