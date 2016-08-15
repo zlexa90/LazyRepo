ce895eabeb2ca629ca87b701c4db2b44578fcb0e
LazyLexa <zlexa90@mail.ru>
2016-08-15 13:53:16 +0300

    Начинаем разработку нового класса Goodbye для обучения

---
+++ GoodbyeWorld.java
@@ -0,0 +1,11 @@
+﻿package helloworld;
+
+/**
+ *
+ * @author Алексей Светлов
+ */
+public class GoodbyeWorld{
+    public static goodbye() {
+        System.out.println("GoodbyeWorld, World!");
+    }    
+}
