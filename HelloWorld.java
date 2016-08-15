fd6e7932e82a54765aa8a731256b46f87edc147d
LazyLexa <zlexa90@mail.ru>
2016-08-15 13:58:52 +0300

    апдейт ветки мастер (написал ещё один принт)

--- HelloWorld.java
+++ HelloWorld.java
@@ -13,5 +13,6 @@ public class HelloWorld {
     public static void main(String[] args) {
         System.out.println("Hello, World!");
 	System.out.println("Hello, Universe!");
+	System.out.println("Hello, All!");
     }    
 }
